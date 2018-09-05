7061aca09807e74342be8acfc55635f1952832cc
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-05 13:13:23 -0600

    add boat class and methods

---
+++ test/boat_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/boat'
+
+class BoatTest < Minitest::Test
+  def test_it_exists
+    kayak = Boat.new(:kayak, 20)
+
+    assert_instance_of Boat, kayak
+  end
+
+  def test_boat_has_a_type
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal :kayak, kayak.type
+  end
+
+  def test_boat_has_a_price_per_hour
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 20, kayak.price_per_hour
+  end
+
+  def test_boat_starts_hours_rented_at_zero
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_boat_rented_hours_can_be_incremented
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 1, kayak.add_hour
+    assert_equal 2, kayak.add_hour
+    assert_equal 3, kayak.add_hour
+    assert_equal 3, kayak.hours_rented
+  end
+
+end
