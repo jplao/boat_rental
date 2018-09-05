14bfbb911979a1da7d0f12767ab733c16e7e41aa
Jenn Lao <jenn.p.lao@gmail.com>
2018-09-05 14:36:59 -0600

    calculate revenue

--- lib/dock.rb
+++ lib/dock.rb
@@ -12,17 +12,24 @@ class Dock
     @boats_rented = []
   end
 
-  def rent_out_boats(boat)
-    @boats_rented << boat
-  end
-
   def rent(boat, renter)
+    @boats_rented << boat
     boat.hours_rented
   end
 
   def log_hour
     boats_rented.each do |boat|
       boat.add_hour
+    end
+  end
+
+  def return(boat)
+    boats_rented.delete(boat)
+  end
+
+  def revenue
+    @boats_rented.sum do |boat|
+      boat.price_per_hour * boat.hours_rented
     end
   end
 end
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -25,9 +25,10 @@ class DockTest < Minitest::Test
     dock = Dock.new("The Rowing Dock", 3)
     kayak_1 = Boat.new(:kayak, 20)
     kayak_2 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
 
-    dock.rent_out_boats(kayak_1)
-    dock.rent_out_boats(kayak_2)
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
 
     assert_equal [kayak_1, kayak_2], dock.boats_rented
   end
@@ -47,8 +48,6 @@ class DockTest < Minitest::Test
     patrick = Renter.new("Patrick Star", "4242424242424242")
     canoe = Boat.new(:canoe, 25)
 
-    dock.rent_out_boats(kayak_1)
-    dock.rent_out_boats(kayak_2)
     dock.rent(kayak_1, patrick)
     dock.rent(kayak_2, patrick)
     dock.log_hour
@@ -56,11 +55,68 @@ class DockTest < Minitest::Test
     assert_equal 1, kayak_1.hours_rented
     assert_equal 1, kayak_2.hours_rented
 
-    dock.rent_out_boats(canoe)
     dock.rent(canoe, patrick)
     dock.log_hour
     assert_equal 2, kayak_1.hours_rented
     assert_equal 2, kayak_2.hours_rented
     assert_equal 1, canoe.hours_rented
   end
+
+  def test_boats_can_be_returned_to_dock
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    canoe = Boat.new(:canoe, 25)
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(canoe, patrick)
+    dock.return(canoe)
+
+    assert_equal [kayak_1, kayak_2], dock.boats_rented
+  end
+
+  def test_dock_can_calculate_revenue
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.log_hour
+    dock.rent(canoe, patrick)
+    dock.log_hour
+    dock.return(kayak_1)
+    dock.return(kayak_2)
+    dock.return(canoe)
+
+    assert_equal 105, dock.revenue
+  end
+
+  def test_dock_can_calculate_revenue
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.log_hour
+    dock.rent(canoe, patrick)
+    dock.log_hour
+    dock.rent(sup_1, eugene)
+    dock.rent(sup_2, eugene)
+    dock.log_hour
+    dock.log_hour
+    dock.log_hour
+  end
+
 end
